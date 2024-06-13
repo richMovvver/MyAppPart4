2d728e024238d768ea7012a3539b3320894a63e2
richMovvver <13736ea@gmail.com>
2024-06-13 23:01:03 +0500

    add

---
+++ app/src/main/java/com/example/myapppart3/data/entities/User.kt
@@ -0,0 +1,11 @@
+package com.example.myapppart3.data.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class User(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val age: Int
+)
